105c570e55a0481b7abb04b8cf764e81c9376934
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-09-02 19:50:01 +0600

    Finished Book archive

--- js/app.js
+++ js/app.js
@@ -1,59 +1,57 @@
-const searchInput = document.getElementById("search-input");
-const searchBtn = document.getElementById("search-btn");
-const bookContainer = document.getElementById("book-container");
-const errorDiv = document.getElementById("error");
-const resultFound = document.getElementById("resultFound");
-const checkData = checkItem => {
-    if (checkItem !== undefined) {
-        return checkItem;
+const searchInput = document.getElementById('search-input');
+const searchBtn = document.getElementById('search-btn');
+const displayBookContainer = document.getElementById('book-container');
+const errorDiv = document.getElementById('error');
+const searchResult = document.getElementById('search-result');
+const checkData = checkDoc => {
+    if (checkDoc !== undefined) {
+        return checkDoc;
     }
     else {
-        return " no data available";
+        return "Data is not available";
     }
 };
 searchBtn.addEventListener("click", function () {
-    const searchValue = searchInput.value;
+    const searchText = searchInput.value;
     //clear data
     searchInput.value = "";
     //Error handling
-    if (searchValue === "") {
-        bookContainer.textContent = "";
-        error.innerText = "Field is empty! please Enter a book name.";
+    if (searchText === "") {
+        displayBookContainer.textContent = "";
+        error.innerText = 'Please type something to search.';
     }
     else {
-        bookContainer.textContent = "";
+        displayBookContainer.textContent = "";
         //load data
-        const url = `https://openlibrary.org/search.json?q=${searchValue}`;
+        const url = `https://openlibrary.org/search.json?q=${searchText}`;
         fetch(url)
             .then(res => res.json())
             .then(data => {
                 console.log(data);
-                if (data.numFound === 0 && searchValue !== "") {
+                if (data.numFound === 0 && searchText !== "") {
                     errorDiv.innerText = "No result Found";
                 }
                 else {
                     errorDiv.innerText = "";
                 }
                 // console.log(data);
-                resultFound.innerHTML = `<h6>result found: ${data.numFound}</h6>`
+                searchResult.innerHTML = `<h3 class="mb-2">Result found: ${data.numFound}</h3>`;
                 data = data.docs;
                 data.forEach(doc => {
                     // console.log(doc);
                     const div = document.createElement("div");
-                    div.classList.add('col-md-3', 'col-12');
-                    const author = checkData(doc.author_name[0]);
-                    const year = checkData(doc.first_publish_year);
-                    const publisher = checkData(doc.publisher[0]);
+                    div.classList.add('col');
                     div.innerHTML = `
-         <div class="border rounded overflow-hidden p-2">
-         <img class="w-100" src="https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg" alt="" />
-       </div>
-       <div class="py-2 d-flex justify-content-between align-items-center d-md-block text-md-center">
-         <h2>Book name: ${doc.title}</h2>
-         <h5>Author Name: ${author} </h5>
-         <h6>publisher: ${publisher}</h6>
-         <h6>First publish Year: ${year}</h6>  </div>`;
-                    bookContainer.appendChild(div);
+                <div class="card h-100">
+                <img class="card-img-top" src="https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg" class="card-img-top" alt="...">
+                <div class="card-body h-25">
+                    <h2 class="card-title">Book Name: ${doc.title}</h2>
+                    <h3 class="card-title">Author Name: ${doc.author_name[0]}</h3>
+                    <h4 class="card-title">Book Publisher: ${doc.publisher[0]}</h4>
+                    <h5 class="card-title">First Publish Year: ${doc.first_publish_year}</h5>
+                </div>
+            </div>`;
+                    displayBookContainer.appendChild(div);
                 });
             });
     }
