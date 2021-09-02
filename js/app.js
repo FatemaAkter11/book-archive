f17fcd2ea3b20b943e916f8fcecd256337e274d7
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-09-02 19:12:16 +0600

    error handling solve

--- js/app.js
+++ js/app.js
@@ -1,57 +1,60 @@
-// book search area
-const searchBook = () => {
-    const searchInputField = document.getElementById('search-input');
-    const searchText = searchInputField.value;
+const searchInput = document.getElementById("searchInput");
+const searchBtn = document.getElementById("search-btn");
+const bookContainer = document.getElementById("book-container");
+const errorDiv = document.getElementById("error");
+const resultFound = document.getElementById("resultFound");
+const checkData = checkItem => {
+    if (checkItem !== undefined) {
+        return checkItem;
+    }
+    else {
+        return " no data available";
+    }
+};
+searchBtn.addEventListener("click", function () {
+    const searchValue = searchInput.value;
     //clear data
-    searchInputField.value = '';
-    if (searchText === '') {
-        displayErrorNotice('Please type something to search.');
-    } else {
-        document.getElementById('error').textContent = '';
-        // load data
-        const url = `http://openlibrary.org/search.json?q=${searchText}`;
-        try {
-            fetch(url)
-                .then(res => res.json())
-                .then(data => displaySearchResult(data.docs));
-        } catch {
-            displayErrorNotice();
-            // console.log(err);
-        }
-
+    searchInput.value = "";
+    //Error handling
+    if (searchValue === "") {
+        bookContainer.textContent = "";
+        error.innerText = "Field is empty! please Enter a book name.";
     }
-}
-
-// display error notice
-const displayErrorNotice = (errorMessage = 'No Result Found') => {
-    document.getElementById('error').innerHTML = `
-		<h5 class="text-danger my-4">${errorMessage}</h5>
-	`;
-}
-
-// book search result
-const displaySearchResult = docs => {
-    // console.log(docs);
-    const searchResult = document.getElementById('search-result');
-    searchResult.textContent = '';
-    if (docs.length === 0) {
-        displayErrorNotice();
+    else {
+        bookContainer.textContent = "";
+        //load data
+        const url = `https://openlibrary.org/search.json?q=${searchValue}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.numFound === 0 && searchValue !== "") {
+                    errorDiv.innerText = "No result Found";
+                }
+                else {
+                    errorDiv.innerText = "";
+                }
+                // console.log(data);
+                resultFound.innerHTML = `<h6>result found: ${data.numFound}</h6>`
+                data = data.docs;
+                data.forEach(doc => {
+                    // console.log(doc);
+                    const div = document.createElement("div");
+                    div.classList.add('col-md-3', 'col-12');
+                    const author = checkData(doc.author_name[0]);
+                    const year = checkData(doc.first_publish_year);
+                    const publisher = checkData(doc.publisher[0]);
+                    div.innerHTML = `
+         <div class="border rounded overflow-hidden p-2">
+         <img class="w-100" src="https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg" alt="" />
+       </div>
+       <div class="py-2 d-flex justify-content-between align-items-center d-md-block text-md-center">
+         <h2>Book name: ${doc.title}</h2>
+         <h5>Author Name: ${author} </h5>
+         <h6>publisher: ${publisher}</h6>
+         <h6>First publish Year: ${year}</h6>  </div>`;
+                    bookContainer.appendChild(div);
+                });
+            });
     }
-    docs.forEach(doc => {
-        // console.log(doc);
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
-                <div class="card h-100">
-                <img class="h-75" src="https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg" class="card-img-top" alt="...">
-                <div class="card-body h-25">
-                    <h2 class="card-title">Book Name: ${doc.title}</h2>
-                    <h3 class="card-title">Author Name: ${doc.author_name}</h3>
-                    <h5 class="card-title">First Publish Year: ${doc.first_publish_year}</h5>
-                </div>
-            </div>
-                `;
-        searchResult.appendChild(div);
-    });
-}
-/* <h4 class="card-title">Book Publisher: ${doc.publisher}</h4> */
+});
